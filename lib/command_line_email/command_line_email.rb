786284a1454765f7e4d13f2b79cd8263b85a0881
James Lavin <james@jameslavin.com>
2012-11-25 11:56:45 -0500

    refactor parse into CleOptparse class

--- lib/command_line_email/command_line_email.rb
+++ lib/command_line_email/command_line_email.rb
@@ -11,55 +11,71 @@ Mail.defaults do
   delivery_method :smtp, user_config.mail_options
 end
 
-mail_attrs = {to: []}
+class CleOptparse
 
-option_parser = OptionParser.new do |opts|
+  def self.parse(args, user_config)
 
-  opts.on("-t TO","--to TO") do |to|
-    mail_attrs[:to] = to_string_to_mailing_list(user_config, to, mail_attrs[:to])
-  end
+    mail_attrs = {to: []}
 
-  opts.on("-f FROM","--from FROM") do |from|
-    mail_attrs[:from] = from
-  end
+    option_parser = OptionParser.new do |opts|
 
-  opts.on("-b BODY","--body BODY") do |body|
-    mail_attrs[:body] ||= ""
-    mail_attrs[:body] << body + "\n"
-  end
+      opts.on("-t TO","--to TO") do |to|
+        mail_attrs[:to] = to_string_to_mailing_list(user_config, to, mail_attrs[:to])
+      end
 
-  opts.on("-s SUBJECT","--subject SUBJECT") do |subject|
-    mail_attrs[:subject] = subject
-  end
+      opts.on("-f FROM","--from FROM") do |from|
+        mail_attrs[:from] = from
+      end
 
-  opts.on("-c CC","--cc CC") do |cc|
-    if user_config.mailing_lists.has_key? cc.to_sym
-      mail_attrs[:cc] = user_config.mailing_lists[cc.to_sym]
-    else
-      mail_attrs[:cc] = cc
+      opts.on("-b BODY","--body BODY") do |body|
+        mail_attrs[:body] ||= ""
+        mail_attrs[:body] << body + "\n"
+      end
+
+      opts.on("-s SUBJECT","--subject SUBJECT") do |subject|
+        mail_attrs[:subject] = subject
+      end
+
+      opts.on("-c CC","--cc CC") do |cc|
+        if user_config.mailing_lists.has_key? cc.to_sym
+          mail_attrs[:cc] = user_config.mailing_lists[cc.to_sym]
+        else
+          mail_attrs[:cc] = cc
+        end
+      end
+
+      opts.on("-f FILE","--add-file FILE") do |file|
+        mail_attrs[:files] ||= []
+        mail_attrs[:files] << file
+      end
+
+      opts.on("-d DIRECTORY","--directory DIRECTORY") do |directory|
+        mail_attrs[:directory] = directory
+      end
+
     end
+
+    option_parser.parse!(args)
+
+    mail_attrs
+  
+  end # parse()
+
+  def self.to_string_to_mailing_list(user_config, to, list=[])
+    # takes optional list in case you want to add to an existing list
+    if user_config.mailing_lists.has_key? to.to_sym
+      Array(user_config.mailing_lists[to.to_sym]).each { |address| list << address }
+    else
+      list << to
+    end
+    list
   end
 
-  opts.on("-f FILE","--add-file FILE") do |file|
-    mail_attrs[:files] ||= []
-    mail_attrs[:files] << file
-  end
+end # class CleOptparse
 
-  opts.on("-d DIRECTORY","--directory DIRECTORY") do |directory|
-    mail_attrs[:directory] = directory
-  end
+mail_attrs = CleOptparse.parse(ARGV, user_config)
 
-end
-
-def to_string_to_mailing_list(user_config, to, list=[])
-  # takes optional list in case you want to add to an existing list
-  if user_config.mailing_lists.has_key? to.to_sym
-    Array(user_config.mailing_lists[to.to_sym]).each { |address| list << address }
-  else
-    list << to
-  end
-  list
-end
+puts mail_attrs
 
 def grab_text_from_filename_if_file_exists(filename_or_text)
   if File.exists?(filename_or_text)
@@ -68,10 +84,6 @@ def grab_text_from_filename_if_file_exists(filename_or_text)
     filename_or_text
   end
 end
-
-option_parser.parse!
-
-puts mail_attrs
 
 mail = Mail.new do
   from    mail_attrs[:from]    || user_config.defaults[:from]
