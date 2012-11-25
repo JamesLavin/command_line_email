c36fd326bba42229b432051f930b985894747a0d
James Lavin <james@jameslavin.com>
2012-11-25 14:54:16 -0500

    remove function I extracted but forgot to remove

--- lib/command_line_email/command_line_email.rb
+++ lib/command_line_email/command_line_email.rb
@@ -17,12 +17,4 @@ mail_attrs = CommandLineEmail::CleOptparse.parse(ARGV, user_config)
 
 puts mail_attrs
 
-def grab_text_from_filename_if_file_exists(filename_or_text)
-  if File.exists?(filename_or_text)
-    File.open(filename_or_text,'r').read
-  else
-    filename_or_text
-  end
-end
-
 CommandLineEmail::DeliverEmail.deliver(user_config, mail_attrs)
