ce172ec4bfee044ffc682b388588fb23213c1dab
James Lavin <james@jameslavin.com>
2012-11-25 10:25:35 -0500

    raise ConfigFileNotFound exception if no email config file

--- lib/command_line_email/send_mail_setup.rb
+++ lib/command_line_email/send_mail_setup.rb
@@ -4,6 +4,13 @@ module CommandLineEmail
 
   class SendMailSetup
 
+    class ConfigFileNotFound < StandardError
+      def initialize(msg)
+        msg ||= "You must provide a mail configuration file. Default location: '~/.command_line_email.yml'"
+        super(msg)
+      end
+    end
+
     attr_reader :mail_options, :mail_config_file
 
     def initialize(config_file = nil)
@@ -22,7 +29,13 @@ module CommandLineEmail
     private
 
     def mail_config
-      @mail_config ||= YAML::load(File.open(mail_config_file))
+      @mail_config ||= load_mail_config_file(mail_config_file)
+    end
+
+    def load_mail_config_file(mail_config_file)
+      YAML::load(File.open(mail_config_file))
+      rescue
+        raise ConfigFileNotFound, nil, [] # turn off ugly backtrace 
     end
 
     def set_mail_options
